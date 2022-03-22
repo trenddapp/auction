73e08916821814e1d37e7b7de698100d4267aaf3
parsaakbari1209 <parsaakbari1209@gmail.com>
2022-03-22 20:21:44 +0430

    feat(frontend): add theme and global styles

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,5 +1,15 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle, ResetCss } from '../styles'
+import { light } from '../theme'
+
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={light}>
+      <ResetCss />
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default App
