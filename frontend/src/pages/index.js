0e5176c6e842d9236f34056b72b8040a7b34dc9b
parsaakbari1209 <parsaakbari1209@gmail.com>
2022-03-22 23:39:42 +0430

    feat(frontend): add banner component

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,27 @@
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
+
+// TODO: Remove dynamic import when web3-react supports ssr.
+const Banner = dynamic(
+  () => {
+    return import('../components/Banner').then((module) => {
+      return module.Banner
+    })
+  },
+  { ssr: false },
+)
+
 const Home = () => {
-  return <div>Home Page</div>
+  return (
+    <>
+      <Head>
+        <title>Auction</title>
+        <meta name="description" content="Decentralized Auction by DAPP-Z" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Banner />
+    </>
+  )
 }
 
 export default Home
