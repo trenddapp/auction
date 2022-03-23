6cc06c40ba040cca37cfa9efd37723ee43e04256
parsaakbari1209 <parsaakbari1209@gmail.com>
2022-03-23 12:29:02 +0430

    feat(frontend): add toast component

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -1,2 +1,3 @@
 export { default as useIsomorphicEffect } from './useIsomorphicEffect'
 export { default as useMatchBreakpoints } from './useMatchBreakpoints'
+export { default as useToast } from './useToast'
