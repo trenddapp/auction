7f8424a4dcf5730a9314758e01e54c81fd5b3433
Mobin <67857772+MobinHajizadeh@users.noreply.github.com>
2022-03-28 20:04:07 +0430

    feat(blockchain): add scripts

--- blockchain/scripts/deploy.py
+++ blockchain/scripts/deploy.py
@@ -1,18 +1,44 @@
-from brownie import Auction, config, network
-from scripts.useful import get_account
+from brownie import Auction, AuctionV2, AuctionProxy, config, Contract, network
+from scripts.useful import encode_function_data, get_account
 
 ACCOUNT = get_account()
 
 
 def deploy_auction():
     auction = Auction.deploy(
-        config["networks"][network.show_active()]["weth"],
         {"from": ACCOUNT},
         publish_source=True,
     )
     return auction
 
 
+def deploy_auction_v2():
+    auction_v2 = AuctionV2.deploy(
+        {"from": ACCOUNT},
+        publish_source=True,
+    )
+    return auction_v2
+
+
+def deploy_proxy(logic):
+    encoded_initializer_function = encode_function_data()
+    proxy = AuctionProxy.deploy(
+        logic,
+        encoded_initializer_function,
+        {"from": ACCOUNT},
+        publish_source=True,
+    )
+    return proxy
+
+
 def main():
     auction = deploy_auction()
     print(f"Auction deployed at {auction} !")
+    auction.initialize(config["networks"][network.show_active()]["weth"])
+
+    proxy = deploy_proxy(auction)
+    print(f"Proxt deployed at {proxy} !")
+    auction_proxy = Contract.from_abi(
+        "Proxy", proxy.address, auction.abi)
+    auction_proxy.initialize(
+        config["networks"][network.show_active()]["weth"], {"from": ACCOUNT})
--- blockchain/scripts/useful.py
+++ blockchain/scripts/useful.py
@@ -1,9 +1,63 @@
 from brownie import accounts, config
 
 
+def encode_function_data(initializer=None, *args):
+    """Encodes the function call so we can work with an initializer.
+    Args:
+        initializer ([brownie.network.contract.ContractTx], optional):
+        The initializer function we want to call. 
+        args (Any, optional):
+        The arguments to pass to the initializer function
+    Returns:
+        [bytes]: Return the encoded bytes.
+    """
+    if not len(args):
+        args = b""
+
+    if initializer:
+        return initializer.encode_input(*args)
+
+    return b""
+
+
 def get_account(index=None, id=None):
     if index:
         return accounts[index]
     if id:
         return accounts.load(id)
     return accounts.add(config["wallets"]["from_key"])
+
+
+def upgrade(
+    account,
+    proxy,
+    newimplementation_address,
+    proxy_admin_contract=None,
+    initializer=None,
+    *args
+):
+    transaction = None
+    if proxy_admin_contract:
+        if initializer:
+            encoded_function_call = encode_function_data(initializer, *args)
+            transaction = proxy_admin_contract.upgradeAndCall(
+                proxy.address,
+                newimplementation_address,
+                encoded_function_call,
+                {"from": account},
+            )
+        else:
+            transaction = proxy_admin_contract.upgrade(
+                proxy.address, newimplementation_address, {"from": account}
+            )
+    else:
+        if initializer:
+            encoded_function_call = encode_function_data(initializer, *args)
+            transaction = proxy.upgradeToAndCall(
+                newimplementation_address, encoded_function_call, {
+                    "from": account}
+            )
+        else:
+            transaction = proxy.upgradeTo(
+                newimplementation_address, {"from": account})
+    return transaction
