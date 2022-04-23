121c5ea81ead806e2874ec2eca3061a27109d713
parsaakbari1209 <parsaakbari1209@gmail.com>
2022-04-23 17:32:43 +0430

    feat(frontend): add nft contract hook

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -1,3 +1,4 @@
+export { default as useContractNft } from './useContractNft'
 export { default as useIsomorphicEffect } from './useIsomorphicEffect'
 export { default as useMatchBreakpoints } from './useMatchBreakpoints'
 export { default as useToast } from './useToast'
