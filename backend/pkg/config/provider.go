5e41f8ad0dbabcf55c07f6bbb7a56680f839ff29
parsaakbari1209 <parsaakbari1209@gmail.com>
2022-05-04 00:14:17 +0430

    feat(backend): accept config from CONFIG env variable

--- backend/pkg/config/provider.go
+++ backend/pkg/config/provider.go
@@ -2,15 +2,18 @@ package config
 
 import (
 	"os"
+	"strings"
 
 	"go.uber.org/config"
 )
 
 func NewYAML() (*config.YAML, error) {
-	options := []config.YAMLOption{}
-	paths := generatePaths("base")
+	options := []config.YAMLOption{
+		config.Expand(os.LookupEnv),
+		config.Source(strings.NewReader(os.Getenv("CONFIG"))),
+	}
 
-	for _, path := range paths {
+	for _, path := range generatePaths("base") {
 		if _, err := os.Stat(path); os.IsNotExist(err) {
 			continue
 		}
