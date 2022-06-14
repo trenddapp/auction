e7f20fa23ced20dc51c90bb42660e8fa4bf4ab8a
Parsa Akbari <parsaakbari1209@gmail.com>
2022-06-14 23:19:06 +0430

    chore(frontend): prefix next specific urls
    Replace '/_next'  URLs with '/auction/_next'.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: '/auction',
   reactStrictMode: true,
+  rewrites() {
+    return [{ source: '/auction/_next/:path*', destination: '/_next/:path*' }]
+  },
 }
 
 module.exports = nextConfig
