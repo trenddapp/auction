2f75fb20c349db8b3e321ce3d388010a2c3f89ab
Parsa Akbari <parsaakbari1209@gmail.com>
2022-06-15 20:03:00 +0430

    chore: remove backend
    The auction backend is moved to a mono-repository.

--- backend/service/nft/clients/etherscan/module.go
+++
@@ -1,8 +0,0 @@
-package etherscan
-
-import "go.uber.org/fx"
-
-var Module = fx.Provide(
-	NewClient,
-	NewConfig,
-)
--- backend/service/nft/http/register.go
+++
@@ -1,8 +0,0 @@
-package http
-
-import "github.com/gin-gonic/gin"
-
-func RegisterRoutes(router *gin.Engine, server *Server) {
-	router.GET("/nft/account/:address", server.GetAccountNFTs)
-	router.GET("/nft/contract/:address", server.GetContractNFTs)
-}
